460e332a722e19c4984f602a7c2adddad3c3be2f
Trần Tiến Lợi <trantienloi2404@gmail.com>
2026-03-15 13:07:18 +0700

    Replace forum mock data by api data (#37)
    * feat(frontend): add logout functionality
    
    * fix(jwt): update JWT expiration values for access and refresh tokens
    
    * refactor(header): simplify admin role checks
    
    * feat(forum): integrate react-query for fetching forum categories
    
    * feat(forum): add custom hooks for fetching forum topics
    
    * feat(forum): add custom hooks for fetching forum posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { RouterProvider } from 'react-router/dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './routes';
 import NotistackProvider from './components/NotistackProvider';
 import ThemeProvider from './theme';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <HelmetProvider>
       <ThemeProvider>
-        <NotistackProvider>
-          <RouterProvider router={router} />
-        </NotistackProvider>
+        <QueryClientProvider client={queryClient}>
+          <NotistackProvider>
+            <RouterProvider router={router} />
+          </NotistackProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
