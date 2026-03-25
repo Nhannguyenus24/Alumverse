7d07c2e323dc4e38cc30b5e54f9268d24ffbb475
Nhân Nguyễn <124347873+Nhannguyenus24@users.noreply.github.com>
2026-03-25 11:40:48 +0700

    feat: impl structure (#53)

--- backend/src/main/java/com/service/backend/forum/dao/ForumCategoryRepository.java
+++ backend/src/main/java/com/service/backend/forum/dao/ForumCategoryRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.service.backend.forum.entities.ForumCategory;
+import com.service.backend.forum.entity.ForumCategory;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
