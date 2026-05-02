d210692b9500bc06884fdd7e5f027422134886aa
TranDucTung <tranductung07012004@gmail.com>
2026-05-02 17:06:05 +0700

    feat(frontend): add new UI for mentorship chat

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -120,6 +120,9 @@ const ActivitiesEventsPage = Loadable(
 const ActivitiesNewsPage = Loadable(
   lazy(() => import("../pages/activities/ActivitiesNewsPage")),
 );
+const NetworkPage = Loadable(
+  lazy(() => import("../pages/network/NetworkPage")),
+);
 
 // Development pages
 const DevelopmentPage = Loadable(
@@ -304,6 +307,14 @@ export const router = createBrowserRouter([
             element: <ActivitiesNewsPage />,
           }
         ],
+      },
+      {
+        path: "network/search",
+        element: <NetworkPage />,
+      },
+      {
+        path: "network/chat",
+        element: <NetworkPage />,
       },
       {
         path: "post",
