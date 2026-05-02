fce5aff79e6c8ccb8f4838dd6e12adb5d844ee03
TranDucTung <tranductung07012004@gmail.com>
2026-05-03 01:40:30 +0700

    chore(frontend): add comment to explain the reason for the usePaginationScrollToTop

--- frontend/src/hooks/usePaginationScrollToTop.js
+++ frontend/src/hooks/usePaginationScrollToTop.js
@@ -8,7 +8,11 @@ export default function usePaginationScrollToTop({
 ) {
   const isFirstRenderRef = useRef(true);
 
-  useEffect(() => {
+  useEffect(() => {  // force to use because if not use, click on the arrow of pagination would not scroll to top, 
+    // only when click to the page number would scroll to top
+    // the reason is because: 
+    // setPage(nextPage);        // React schedules re-render (async)
+    // window.scrollTo({ ... }); // run immediately - at this time page has not been actually changed
     if (isFirstRenderRef.current) {
       isFirstRenderRef.current = false;
       return;
