aa843f0ef16f4f16a1d5f1d1ec4808de9513f422
tdt-hcmus <122892121+tranductung07012004@users.noreply.github.com>
2026-07-05 21:58:30 +0700

    feat(frontend): change effect of verification 2 on chat features (#307)

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -10,6 +10,7 @@ import LoadingScreen from "../components/LoadingScreen";
 import { Loadable, AuthLoadable } from "./loadable";
 import MentorshipFullAccessGate from "../components/mentorship/MentorshipFullAccessGate";
 import MentorshipBookingGate from "../components/mentorship/MentorshipBookingGate";
+import ChatAccessGate from "../components/network/ChatAccessGate";
 
 if (typeof window !== "undefined") {
   queueMicrotask(() => {
@@ -438,7 +439,9 @@ export const router = createBrowserRouter([
         handle: { hideFooter: true },
         element: (
           <ProtectedRoute>
-            <ChatPage />
+            <ChatAccessGate>
+              <ChatPage />
+            </ChatAccessGate>
           </ProtectedRoute>
         ),
       },
